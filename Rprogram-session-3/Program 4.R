ac701a43948cb2c6a092d087b22e230fe324e7b5
navya-datascience <navyadn2006@gmail.com>
2026-07-17 09:55:54 +0530

    Program 4

---
+++ Rprogram-session-3/Program 4.R
@@ -0,0 +1,9 @@
+# Execute this using %/% operator 
+
+135 %/% 60   # Result: 2 (hours)
+135 %% 60    # Result: 15 (minutes leftover)
+# Together, it tells you 135 minutes is 2 hours and 15 minutes!
+
+47 %/% 12   # Result: 3 (You can fill 3 complete cartons)
+47 %% 12    # Result: 11 (You have 11 loose eggs left over)
+
